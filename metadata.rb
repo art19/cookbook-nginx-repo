c75993378618dec6446fbee4ead1f2d8c2d2be79
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-08-21 02:34:18 +0000

    pin cookbook dependencies whole versions

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description 'Configures NGINX Vendor-Specific Repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-depends 'apt', '~> 2.8.1'
-depends 'yum', '~> 3.6.3'
+depends 'apt', '~> 2.8'
+depends 'yum', '~> 3.6'
 
 supports 'rhel', '>= 5', '< 8'
 supports 'centos', '>= 5', '< 8'
