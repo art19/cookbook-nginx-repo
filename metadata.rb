ebe762e4afc2df7a4fb6d2fc8800c000c4d89877
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-08-21 22:33:31 +0000

    swap order of urls and add support source

--- metadata.rb
+++ metadata.rb
@@ -6,12 +6,14 @@ description 'Configures NGINX Vendor-Specific Repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-issues_url 'https://github.com/st-isidore-de-seville/cookbook-nginx-repo/issues'
 source_url 'https://github.com/st-isidore-de-seville/cookbook-nginx-repo'
+issues_url 'https://github.com/st-isidore-de-seville/cookbook-nginx-repo/issues'
 
 depends 'apt', '~> 2.8'
 depends 'yum', '~> 3.6'
 
+# Supported Platforms:
+#  http://nginx.org/en/linux_packages.html
 supports 'rhel', '>= 5', '< 8'
 supports 'centos', '>= 5', '< 8'
 supports 'debian', '>= 6', '< 9'
