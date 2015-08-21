e5ac0392603de5dddeb11392951b87eda0dd7f65
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-08-21 02:36:05 +0000

    add github urls

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description 'Configures NGINX Vendor-Specific Repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
+issues_url 'https://github.com/st-isidore-de-seville/cookbook-nginx-repo/issues'
+source_url 'https://github.com/st-isidore-de-seville/cookbook-nginx-repo'
+
 depends 'apt', '~> 2.8'
 depends 'yum', '~> 3.6'
 
