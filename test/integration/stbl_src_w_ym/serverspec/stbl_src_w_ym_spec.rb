fb6bb9853677569df92a20c4cd27178f279acf5c
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-08-24 23:31:46 -0400

    better commit

--- test/integration/stbl_src_w_ym/serverspec/stbl_src_w_ym_spec.rb
+++ test/integration/stbl_src_w_ym/serverspec/stbl_src_w_ym_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
+# the assumption made here is that if there is a source repo present, then the
+#  the source package was downloaded successfully
 describe command('ls /nginx-*.ngx.src.rpm') do
   its(:exit_status) { should eq 0 }
 end
