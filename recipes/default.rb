181640d59c984d55798b29b5258a72c083eeedd8
St. Isidore de Seville <st.isidore.de.seville@gmail.com>
2015-08-26 04:38:22 +0000

    test for unsupported platform families

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,6 @@
 #  The default recipe installs & configures NGINX repositories per
 #  http://nginx.org/en/linux_packages.html for apt and yum.
 
+fail('platform family is unsupported') unless platform_family?('debian', 'rhel')
 include_recipe 'nginx-repo::_apt' if platform_family?('debian')
 include_recipe 'nginx-repo::_yum' if platform_family?('rhel')
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe 'nginx-repo' do
+  describe 'when on an unsupported platform' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new(platform: 'gentoo', version: '2.1')
+        .converge(described_recipe)
+    end
+
+    it 'should raise an error' do
+      expect { chef_run }.to raise_error('platform family is unsupported')
+    end
+  end
+
   describe 'when on a debian platform family' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '12.04')
